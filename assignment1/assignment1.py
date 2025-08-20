c04fabc1d37917d39cb3b4552b2f5c5e01918558
syeda shakera <unisyeds808@gmail.com>
2025-08-20 10:02:20 -0600

    changes

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -25,13 +25,6 @@ def greet(name):
 greet("Shakera")
 
 print("********************TASK3**************")
-#Write a calc function. It takes three arguments.
-#  The default value for the third argument is "multiply".
-#  The first two arguments are values that are to be combined using the 
-# operation requested by the third argument,
-#  a string that is one of the following add, subtract, multiply, 
-# divide, modulo, int_divide (for integer division) and power. 
-# The function returns the result.
 def calc(x, y, z="multiply"): 
     try:
         match z:
